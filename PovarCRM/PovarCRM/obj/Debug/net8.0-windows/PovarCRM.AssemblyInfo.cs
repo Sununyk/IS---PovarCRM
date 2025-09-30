0ba484dca77ab5543e89b420d19a045204a60de5
Александр <alexuliz@mail.ru>
2025-09-30 23:45:53 +0600

    Otchet

--- PovarCRM/PovarCRM/obj/Debug/net8.0-windows/PovarCRM.AssemblyInfo.cs
+++ PovarCRM/PovarCRM/obj/Debug/net8.0-windows/PovarCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PovarCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dee5285265f1e7002b1dfc467da053dc831e40d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e76f7df18000e817ba0c5568205b4bf6651ba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PovarCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PovarCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
